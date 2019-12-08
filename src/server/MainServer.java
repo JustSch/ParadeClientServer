e68d04f30b98506eb60d50ee4b1f87310bfca8c8
Justin Schreiber <jschreiber234@gmail.com>
2019-12-07 21:20:18 -0500

    changes

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -1,9 +1,40 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
-	public static void main(String[] args) {
-		
-		
+
+	private static int port;
+	private static String address;
+
+	public MainServer() {
+
+		try {
+			ServerSocket server = new ServerSocket(5000);
+			//InetAddress myAddress = InetAddress.getLocalHost();
+			
+			System.out.println("Wating for Client");
+
+			Socket connection = server.accept();
+			DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+			DataInputStream dis = new DataInputStream(connection.getInputStream());
+			
+			//spawn helper then pass streams into helper
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
+	public static void main(String[] args) {
+		System.out.println("s");
+	}
+
 }
